ddbacc34f64169d7e8368108754f3116144dcbc0
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-08-16 09:01:14 -0700

    connected geolocation

---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LoggingKata;
+
+namespace LoggingKata
+{
+    class TacoBell : ITrackable
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+
+    }
+
+}
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,27 +18,38 @@
             if (cells.Length < 3)
             {
                 // Log that and return null
+                logger.LogError("Error");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
             // grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
+
             // grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
+
             // grab the name from your array at index 2
+            var name = cells[2];
 
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
 
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
 
-            // Then, you'll need an instance of the TacoBell class
+            TacoBell store = new TacoBell();
+
             // With the name and point set correctly
+            //seeting name
+             store.Name = name;
 
-            // Then, return the instance of your TacoBell class
-            // Since it conforms to ITrackable
+            //setting point
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
 
-            return null;
+            //updating points
+            store.Location = point;
+
+
+            return store;
         }
     }
 }
